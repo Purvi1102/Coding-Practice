aaaa32a2c514b5895339be17d47406300d4426e3
Purvi1102 <purvi.0605@gmail.com>
2025-05-27 15:02:46 +0530

    Sum of digits

---
+++ 27may2025/sumOfDigits.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    cout<< "enter first digit : ";
+    cin>> num1;
+    cout<< " enter second digit : ";
+    cin>> num2;
+    cout<< num1+num2;
+    return 0;
+}
