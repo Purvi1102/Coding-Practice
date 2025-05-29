a981dfa6414ed6e6a7b51d0bb06044251f59de27
Purvi1102 <purvi.0605@gmail.com>
2025-05-29 13:49:08 +0530

    compression of string

---
+++ 29may2025/compressingString.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    int length_of_string;
+    cout<< "enter the length of string";
+    cin>> length_of_string;
+    string str1;
+    cout<< "enter the string: ";
+    cin>> str1;
+    int count=1;
+    string ans;
+    for(int i=0;i<length_of_string-1;i++)
+    {
+        if(str1[i]==str1[i+1]) count++;
+        else 
+        {
+            ans.push_back(str1[i]);
+            string c=to_string(count);
+            ans+= c;
+            count=1;
+        }
+    }
+    if(str1[length_of_string-1]!=str1[length_of_string-2]) 
+    {
+        ans+=str1[length_of_string-1];
+        ans+="1";
+    }
+    cout<< "compressed string is :" << ans;
+
+}
