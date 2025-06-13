f2efd2a565c8b5d48e29dcf8628ecdb5dd47cc3e
Purvi1102 <purvi.0605@gmail.com>
2025-06-13 23:24:36 +0530

    sum

---
+++ 8june2025/coolPat.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+int main()
+{
+    int test_cases;
+    cin >> test_cases;
+    while (test_cases--)
+    {
+        int n ;
+       cin>> n;
+        int x;
+        cin>> x ;
+        vector<int> arr1(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> arr1[i];
+        }
+        for(int i)
+    }
+}
